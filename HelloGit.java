21518b7216c631b47efccc9d3630e328b2714aa5
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-14 10:14:42 +0200

    Hello Git

---
+++ HelloGit.java
@@ -0,0 +1,10 @@
+package be.intecbrussel.helloGit;
+
+public class HelloGit
+{
+    public static void main(String[] args)
+    {
+        String salutation = "Hello Git";
+        System.out.println(salutation);
+    }
+}
